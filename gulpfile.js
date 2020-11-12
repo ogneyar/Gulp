76843ee83bbffc550d7e0a42c7e0626703546590
ogneyar <ogneyar-ne@ya.ru>
2020-11-12 18:54:36 +0300

    qqq

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-let project_folder = "./public";
-let source_folder = ".";
+let project_folder = "dist";
+let source_folder = "#src";
 
 let path = {
     build: {
@@ -10,7 +10,7 @@ let path = {
         fonts: project_folder + "/fonts/",
     },
     src: {
-        html: source_folder + "/*.html",
+        html: [source_folder + "/*.html", "!"+source_folder + "/_*.html"],
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/script.js",
         img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
@@ -27,28 +27,60 @@ let path = {
 }
 
 let { src, dest } = require('gulp');
+let gulp = require('gulp');
+let browsersync = require('browser-sync').create();
+let fileinclude = require('gulp-file-include');
+let del = require('del');
+let scss = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
 
-var gulp = require('gulp');
-/*
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var nodemon = require('gulp-nodemon');
-*/
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: "./" + project_folder + "/"
+        },
+        port: 8000,
+        notify: false
+    })
+}
 
 function watchFiles() {
     gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
 }
 
 function html() {
     return src(path.src.html)
-        .pipe(gulp.dest(path.build.html))
+        .pipe(fileinclude())
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream())
 }
 
-let build = gulp.series(html);
-let watch = gulp.parallel(build, watchFiles, nodeM);
+function css() {
+    return src(path.src.css)
+        .pipe(
+            scss({
+                outputStyle: "expanded"
+            })
+        ),
+        pipe(
+            autoprefixer({
+                overrideBrowserlist: ["last 5 version"],
+                cascade: true
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())
+}
 
+function clean() {
+    return del(path.clean);
+}
+
+let build = gulp.series(clean, gulp.parallel(css, html));
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
